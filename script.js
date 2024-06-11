76688ce29fed6b9c1cd1405479edb7bff54aec07
wendelllopess <wendelllope020@gmail.com>
2024-06-11 09:59:02 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const skatista = document.querySelector('.skatista');
 const bloco = document.querySelector('.bloco');
 
+const contadorElement = document.getElementById('contador');
+const jogo = document.querySelector('.jogo');
+
+let contador = 0;
+let passouBloco = false;
+let trocaDeCores = false;
+
+skatista.style.bottom = '-20px';
+
+
 //funcao para fazer com que o skatista pule 
 const pulo = () => {
     //ira adicionar a classe do css que contem a animacao para o skatista pular 
@@ -26,7 +36,7 @@ const loop = setInterval( ()=>{
     //verifica se o bloco esta chegando perto do skatista e para o bloco na posicao da colisao 
    // e tambem verifica se o skatista pulou e se foi maior que o tamano do bloco
 
-    if(posicaoBloco <= 160 && posicaoBloco > 0 && posicacoSkatista <110){
+    if(posicaoBloco <= 120 && posicaoBloco > 0 && posicacoSkatista <100){
         bloco.style.animation = 'none';
         bloco.style.left = `${posicaoBloco}px`;
 
@@ -40,12 +50,34 @@ const loop = setInterval( ()=>{
 
         //ira para a funcao do loop quando perder, e nao deixar que o jogo rodando mesmo quadandon der game over
         clearInterval(loop);
+    } else if (posicaoBloco < 0 && !passouBloco) {
+        contador++;
+        contadorElement.textContent = contador;
+        passouBloco = true;
+        trocarCorFundo(); // Chamar a função para trocar a cor do fundo
+    } else if (posicaoBloco > 160) {
+        passouBloco = false;
     }
-
-    
 
 },16)
 
 
-
 document.addEventListener('keydown', pulo);
+
+// Função para gerar uma cor RGB aleatória
+const corAleatoria = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+// Função para alternar a cor do background
+const trocarCorFundo = () => {
+    if (contador >= 10 && !trocaDeCores) {
+        trocaDeCores = true;
+        setInterval(() => {
+            jogo.style.backgroundColor = corAleatoria();
+        }, 1000); // Muda a cor a cada segundo
+    }
+}
